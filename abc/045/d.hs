e156643774b234c1d0f7d69f31fcaa876f2d3063
naoyoshi <naoyoshi0511@gmail.com>
2016-09-19 16:05:39 +0900

    a

--- abc/045/d.hs
+++ abc/045/d.hs
@@ -3,15 +3,26 @@ import Control.Applicative
 import Data.List
 import Debug.Trace
 
-x -: f = f x
+dbg v = traceShow v v
 
-points [a, b] = (,) <$> range a <*> range b
-    where range = filter (>= 0) . ((+) <$> [-2..0] <*>) . pure
+points h w [a, b] = (,) <$> range h a <*> range w b
+    where range max = filter (cond max) . ((+) <$> [-2..0] <*>) . pure
+          cond max  = (&&) <$> (>= 0) <*> (<= max - 3)
 
---solve ls = sort . map length . group . sort $ points =<< ls
-solve ls = (ls >>= points) -: sort -: group -: map length -: sort
+frequencies :: Ord a => [a] -> [(a, Int)]
+frequencies = map keyAndLen . group . sort
+    where keyAndLen grp = (head grp, length grp)
+
+-- [(0,0), (0,0), (1,1), (1,1), (1, 2)]
+-- [2, 2, 1]
+-- [(1, 1), (2, 2)]
+solve h w ls = freqs2
+    where ps       = dbg $ ls >>= points h w
+          freqs    = dbg $ frequencies $ ps
+          freqs2   = dbg $ frequencies . filter (<= 9) . map snd $ freqs
+          mp       = 
 
 main = do
     [h, w, _] :: [Int] <- map read . words <$> getLine
-    ls :: [[Int]]      <- map (map read . words) . lines <$> getContents
-    print $ solve ls
+    ls :: [[Int]]      <- map (map (pred . read) . words) . lines <$> getContents
+    print $ solve h w ls
